494a9db7215ef40c74ef02a524cb9796c3911dc1
stonasss <luis.b.santos96@gmail.com>
2022-10-17 12:04:33 -0300

    html fix, ajustes ao .js e redirecionamento ao whatsapp aplicado

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
     let chosen_food;
     let chosen_drink;
     let chosen_desert;
-    let name;
-    let address;
+    let food_name = document.querySelector(".selected-food");
+    let drink_name = document.querySelector(".selected-drink");
+    let desert_name = document.querySelector(".selected-desert");
     let deactivated_button = document.getElementById("unclick")
 
 function chooseFood(food) {
@@ -47,7 +48,6 @@ function activatePayment() {
         const confirmOrderText = document.querySelector(".button");
         confirmOrderText.innerHTML ="Fechar pedido";
         confirmOrderText.classList.add("button-selected");
-        deactivated_button.classList.remove("button-off");
     }
     }
     }
@@ -56,5 +56,27 @@ function activatePayment() {
 function makeOrder() {
     const name = prompt("Qual o seu nome?");
     const address = prompt ("E o seu endereço?");
+    const food_name = (document.querySelector(".selected-food>h1")).innerHTML;
+    const drink_name = (document.querySelector(".selected-drink>h1")).innerHTML;
+    const desert_name = (document.querySelector(".selected-desert>h1")).innerHTML;
+    
+    let total = 0;
+    let cost1 = (document.querySelector(".selected-food>h3")).innerHTML.replace("R$ ", "");
+    cost1 = cost1.replace(",", ".");
+    cost1 = Number(cost1);
+    total += cost1;
 
+    cost2 = (document.querySelector(".selected-drink>h3")).innerHTML.replace("R$ ", "");
+    cost2 = cost2.replace(",", ".");
+    cost2 = Number(cost2);
+    total += cost2;
+
+    cost3 = (document.querySelector(".selected-desert>h3")).innerHTML.replace("R$ ", "");
+    cost3 = cost3.replace(",", ".");
+    cost3 = Number(cost3);
+    total += cost3;
+
+    let msg = `Olá, gostaria de fazer o seguinte pedido:\n- Prato: ${food_name}\n- Bebida: ${drink_name}\n- Desert: ${desert_name}\n- Total: R$ ${total.toFixed(2)}\n- Nome: ${name}\n- Endereço: ${address}`;
+    msg = encodeURIComponent(msg);
+    window.open("https://wa.me/5521996790244?text=" + msg);
 }
