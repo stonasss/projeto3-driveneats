c3c2ecaeb24ae8ab14f7d2dd34648530583aeee3
stonasss <luis.b.santos96@gmail.com>
2022-10-16 22:03:50 -0300

    Ajustes ao html/css & js, funcionalidades adicionadas ao botão

--- script.js
+++ script.js
@@ -1,89 +1,60 @@
-function chooseFrango() {
-    const frango = document.getElementById("frango");
-    frango.classList.toggle("selected");
-    yesfrango.classList.toggle("icon");
-    yesstrogonofe.classList.add("icon");
-    yespratofeito.classList.add("icon");
-    strogonofe.classList.remove("selected");
-    pratofeito.classList.remove("selected");
+    let chosen_food;
+    let chosen_drink;
+    let chosen_desert;
+    let name;
+    let address;
+    let deactivated_button = document.getElementById("unclick")
+
+function chooseFood(food) {
+    const food_selected = document.querySelector(".selected-food");
+    if (food_selected !== null){
+        food_selected.classList.remove("selected-food");
+    }    
+        food.classList.add("selected-food");
+
+        chosen_food = food.innerHTML;
+        activatePayment();
 }
 
-function chooseStrogonofe() {
-    const strogonofe = document.getElementById("strogonofe");    
-    strogonofe.classList.toggle("selected");
-    yesstrogonofe.classList.toggle("icon");
-    yesfrango.classList.add("icon");
-    yespratofeito.classList.add("icon");
-    frango.classList.remove("selected"); 
-    pratofeito.classList.remove("selected");   
+function chooseDrink(drink) {
+    const drink_selected = document.querySelector(".selected-drink")
+
+    if (drink_selected !== null){
+        drink_selected.classList.remove("selected-drink");
+    }    
+        drink.classList.add("selected-drink");
+
+        chosen_drink = drink.innerHTML;
+        activatePayment();
 }
 
-function choosePratoFeito() {
-    const pratofeito = document.getElementById("pratofeito");   
-    pratofeito.classList.toggle("selected");
-    yespratofeito.classList.toggle("icon");
-    yesstrogonofe.classList.add("icon");
-    yesfrango.classList.add("icon"); 
-    strogonofe.classList.remove("selected");
-    frango.classList.remove("selected");
+function chooseDesert(desert) {
+    const desert_selected = document.querySelector(".selected-desert")
+
+    if (desert_selected !== null){
+        desert_selected.classList.remove("selected-desert");
+    }    
+        desert.classList.add("selected-desert");
+
+        chosen_desert = desert.innerHTML;
+        activatePayment();
 }
 
-function chooseCoquinha() {
-    const coquinha = document.getElementById("coquinha");
-    coquinha.classList.toggle("selected");
-    yescoquinha.classList.toggle("icon");
-    yesmatte.classList.add("icon");
-    yesagua.classList.add("icon");
-    matte.classList.remove("selected");
-    agua.classList.remove("selected");
+function activatePayment() {
+    if (chosen_food !== undefined){
+    if (chosen_drink !== undefined){
+    if (chosen_desert !== undefined){
+        const confirmOrderText = document.querySelector(".button");
+        confirmOrderText.innerHTML ="Fechar pedido";
+        confirmOrderText.classList.add("button-selected");
+        deactivated_button.classList.remove("button-off");
+    }
+    }
+    }
 }
 
-function chooseMatte() {
-    const matte = document.getElementById("matte");
-    matte.classList.toggle("selected");
-    yesmatte.classList.toggle("icon");
-    yescoquinha.classList.add("icon");
-    yesagua.classList.add("icon");
-    coquinha.classList.remove("selected");
-    agua.classList.remove("selected");
-}
+function makeOrder() {
+    const name = prompt("Qual o seu nome?");
+    const address = prompt ("E o seu endereço?");
 
-function chooseAgua() {
-    const agua = document.getElementById("agua");
-    agua.classList.toggle("selected");
-    yesagua.classList.toggle("icon");
-    yescoquinha.classList.add("icon");
-    yesmatte.classList.add("icon");
-    matte.classList.remove("selected");
-    coquinha.classList.remove("selected");
-}
-
-function chooseTorta() {
-    const torta = document.getElementById("torta");
-    torta.classList.toggle("selected");
-    yestorta.classList.toggle("icon");
-    yespudim.classList.add("icon");
-    yesbrigadeiro.classList.add("icon");
-    pudim.classList.remove("selected");
-    brigadeiro.classList.remove("selected");
-}
-
-function choosePudim() {
-    const pudim = document.getElementById("pudim");
-    pudim.classList.toggle("selected");
-    yespudim.classList.toggle("icon");
-    yestorta.classList.add("icon");
-    yesbrigadeiro.classList.add("icon");
-    torta.classList.remove("selected");
-    brigadeiro.classList.remove("selected");
-}
-
-function chooseBrigadeiro() {
-    const brigadeiro = document.getElementById("brigadeiro");
-    brigadeiro.classList.toggle("selected");
-    yesbrigadeiro.classList.toggle("icon");
-    yestorta.classList.add("icon");
-    yespudim.classList.add("icon");
-    torta.classList.remove("selected");
-    pudim.classList.remove("selected");
 }
